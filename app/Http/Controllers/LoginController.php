e5a641de4c5e3be7a3d73ba88717814326112b46
Yi Zhou <zhouyi0424@gmail.com>
2018-06-22 00:12:16 +1200

    achieve the login page

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,12 +16,27 @@ class LoginController extends Controller
     //login behaviour
 
     public function login() {
+        //validate
+        $this->validate(request(),[
+            'email' => 'required|email',
+            'password' => 'required|min:5|max:10',
+            'is_remember' => 'integer',
+        ]);
+
+        //logic
+        $user = request(['email', 'password']);
+        $is_remember = boolval(request('is_remember'));
+        if(\Auth::attempt($user,$is_remember)) {
+            return redirect('/posts');
+        }
+        //render
+        return \Redirect::back() -> withErrors('Email address is not matched with password');
 
     }
 
     //logout  behaviour
     public function logout() {
-
-
+        \Auth::logout();
+        return redirect('/login');
     }
 }
