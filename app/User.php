04813cc4f031d8c68608d68f1eae77b16313e11c
Yi Zhou <zhouyi0424@gmail.com>
2018-06-21 23:23:55 +1200

    use Auth for User model

--- app/User.php
+++ app/User.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use App\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 //table => users
-class User extends Model
+class User extends Authenticatable
 {
-    //
+    protected $fillable = [
+        'name', 'email', 'password'];
 }
