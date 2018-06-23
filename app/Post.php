e59ffe5ec8f0c7424dce58c5f8faae9863c55464
Yi Zhou <zhouyi0424@gmail.com>
2018-06-23 23:07:35 +1200

    create comment model

--- app/Post.php
+++ app/Post.php
@@ -7,8 +7,13 @@ use App\Model;
 //table => posts
 class Post extends Model
 {
-// Related to user
+    // Related to user
     public function user() {
         return $this->belongsTo('App\User');
+    }
+
+    //comment model
+    public function comments() {
+        return $this->hasMany('App\Comment') -> orderBy('created_at', 'desc');
     }
 }
