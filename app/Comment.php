89e27bcf9bcfb013447238351ba1024e3e69d607
Yi Zhou <zhouyi0424@gmail.com>
2018-06-24 21:39:35 +1200

    finish the comments list

--- app/Comment.php
+++ app/Comment.php
@@ -7,8 +7,14 @@ use App\Model;
 class Comment extends Model
 {
     //comment the article
-
     public function post(){
         return $this->belongsTo("App\Post");
     }
+
+    //comment user
+
+    public function user() {
+        return $this->belongsTo("App\User");
+    }
+
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
 
     // article page
     public function show(Post $post) {
-
+        $post->load('comments');
         return view("post/show", compact('post'));
 
     }
