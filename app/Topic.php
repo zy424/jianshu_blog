f0723067ad6d3722918933ce4b8253d44d2184c7
Yi Zhou <zhouyi0424@gmail.com>
2018-06-30 23:31:38 +1200

    build Topic model

--- app/Topic.php
+++ app/Topic.php
@@ -6,5 +6,13 @@ use App\Model;
 
 class Topic extends Model
 {
-    //
+    //the all articles that belong to the topic
+    public function posts() {
+        $this->belongsToMany(\App\Post::class, 'post_topics', 'topic_id', 'post_id');
+    }
+
+    //the number of articles that belong to the topic
+    public function postTopics() {
+        $this->hasMany(\App\PostTopic::class, 'topic_id');
+    }
 }
