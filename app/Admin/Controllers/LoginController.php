21d83676a0a067499b665e3f4a2d73806afd7cd4
Yi Zhou <zhouyi0424@gmail.com>
2018-07-02 23:09:20 +1200

    build backend management platform

--- app/Admin/Controllers/LoginController.php
+++ app/Admin/Controllers/LoginController.php
@@ -16,10 +16,26 @@ class LoginController extends Controller {
     }
 
     public function login() {
+        //validate
+        $this->validate(request(),[
+            'name' => 'required|min:2',
+            'password' => 'required|min:5|max:10',
+
+        ]);
+
+        //logic
+        $user = request(['name', 'password']);
+        if(\Auth::guard('admin')->attempt($user)) {
+            return redirect('/admin/home');
+        }
+
+        //render
+        return \Redirect::back() -> withErrors('Name is not matched with password');
 
     }
 
     public function logout() {
-
+        \Auth::guard('admin')->logout();
+        return redirect('/admin/login');
     }
 }
--- resources/views/admin/login/index.blade.php
+++ resources/views/admin/login/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Log in</title>
+    <title>yiBlog Admin | Log in</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
@@ -28,27 +28,28 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="/adminlte/index2.html">ylaravel管理后台</a>
+        <a href="/adminlte/index2.html">yiBlog Backend Management Platform</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
-        <p class="login-box-msg">登陆</p>
+        <p class="login-box-msg">Log in</p>
 
         <form action="/admin/login" method="post">
-            <input type="hidden" name="_token" value="RPPMc0lhvtynKELDZljXlz9UZI9uNc55ip1P8GCM">
+            {{csrf_field()}}
             <div class="form-group has-feedback">
-                <input name="name" type="text" class="form-control" placeholder="名字">
+                <input name="name" type="text" class="form-control" placeholder="Name">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input name="password" type="password" class="form-control" placeholder="密码">
+                <input name="password" type="password" class="form-control" placeholder="Password">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="row">
                 <!-- /.col -->
                 <div class="col-xs-4">
-                    <button type="submit" class="btn btn-primary btn-block btn-flat">登陆</button>
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">Login</button>
                 </div>
+                @include('admin.layout.error')
                 <!-- /.col -->
             </div>
         </form>
