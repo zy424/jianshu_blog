48d6308e4f1a1512625051eb0dde3f9e00416c55
Yi Zhou <zhouyi0424@gmail.com>
2018-07-09 23:15:48 +1200

    build notices table and notice model

--- app/User.php
+++ app/User.php
@@ -12,30 +12,35 @@ class User extends Authenticatable
         'name', 'email', 'password'];
 
     //get the articles list of the user
-    public function posts() {
+    public function posts()
+    {
         return $this->hasMany(\App\Post::class, 'user_id', 'id');
     }
 
     //get the fans of the user
-    public function fans() {
+    public function fans()
+    {
         return $this->hasMany(\App\Fan::class, 'star_id', 'id');
     }
 
     //get the star of the user focuses
 
-    public function stars(){
+    public function stars()
+    {
         return $this->hasMany(\App\Fan::class, 'fan_id', 'id');
     }
 
     //the uses want to focus somebody
-    public function doFan($uid) {
+    public function doFan($uid)
+    {
         $fan = new \App\Fan();
         $fan->star_id = $uid;
         return $this->stars()->save($fan);
     }
 
     //delete the focus
-    public function doUnFan($uid) {
+    public function doUnFan($uid)
+    {
         $fan = new \App\Fan();
         $fan->star_id = $uid;
         $this->stars()->delete($fan);
@@ -43,14 +48,26 @@ class User extends Authenticatable
 
     //if the user has been focused by the fan
 
-    public function hasFan($uid) {
+    public function hasFan($uid)
+    {
         return $this->fans()->where('fan_id', $uid)->count();
     }
 
     //if the user focuses a star
-    public function hasStar($uid) {
+    public function hasStar($uid)
+    {
         return $this->stars()->where('star_id', $uid)->count();
     }
 
+    //用户收到的通知
+    public function notices()
+    {
+        return $this->belongsToMany(\App\Notice::class, 'user_notice', 'user_id', 'notice_id')
+            ->withPivot(['user_id', 'notice_id']);
+    }
 
+    //给用户增加通知
+    public function addNotice($notice) {
+        return $this->notices()->save($notice);//delete 使用 detach()
+    }
 }
